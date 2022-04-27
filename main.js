81f2287bcdd836961201a9da7e2bde669cf01118
sandeep-19 <sandeep.rqs@gmail.com>
2022-04-27 14:50:48 +0530

    commit on main

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 (function () {
   "use strict";
-  /** countdown**/
+  /** countdown for event**/
   // Set the date we're counting down to
-  var countDownDate = new Date("may 7, 2022 08:00:00").getTime();
+  var countDownDate = new Date("may 10, 2022 08:00:00").getTime();
 
   // Update the count down every 1 second
   var x = setInterval(function () {
@@ -22,12 +22,43 @@
 
     // Output the result in an element with id="demo"
     document.getElementById("time").innerHTML =
-      days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+      days + "D " + hours + "h " + minutes + "m " + seconds + "s ";
 
     // If the count down is over, write some text
     if (distance < 0) {
       clearInterval(x);
       document.getElementById("time").innerHTML = "EXPIRED";
+    }
+  }, 1000);
+
+  /** countdown for registration**/
+
+  var countDownDate = new Date("may 4, 2022 20:00:00").getTime();
+
+  // Update the count down every 1 second
+  var x = setInterval(function () {
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Output the result in an element with id="demo"
+    document.getElementById("time").innerHTML =
+      days + "D " + hours + "h " + minutes + "m " + seconds + "s ";
+
+    // If the count down is over, write some text
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("tim").innerHTML = "EXPIRED";
     }
   }, 1000);
 
